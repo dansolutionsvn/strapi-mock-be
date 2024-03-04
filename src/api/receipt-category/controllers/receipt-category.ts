6816c31ed78ccdb52e3de69010d9c14ece8a8c3a
linh2001 <76433644+leanhlinh0108@users.noreply.github.com>
2024-03-04 12:20:56 +0700

    chore:

---
+++ src/api/receipt-category/controllers/receipt-category.ts
@@ -0,0 +1,7 @@
+/**
+ * receipt-category controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::receipt-category.receipt-category');
---
+++ src/api/receipt-category/services/receipt-category.ts
@@ -0,0 +1,7 @@
+/**
+ * receipt-category service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::receipt-category.receipt-category');
